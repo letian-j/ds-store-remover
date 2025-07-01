f307f139790fd5f03250d17f84f8b9ed2c51e07c
Letian Jiang <144998425+LetianJiang2@users.noreply.github.com>
2025-07-01 14:26:25 +0800

    Add LICENSE file to the repository

--- DS-Store-Remover/FileCleaner.swift
+++ DS-Store-Remover/FileCleaner.swift
@@ -4,7 +4,7 @@ struct FileCleaner {
     static func clean(folder: URL) async throws {
         let fileManager = FileManager.default
         let resourceKeys: [URLResourceKey] = [.isDirectoryKey]
-        let enumerator = fileManager.enumerator(at: folder, includingPropertiesForKeys: resourceKeys, options: [.skipsHiddenFiles])
+        let enumerator = fileManager.enumerator(at: folder, includingPropertiesForKeys: resourceKeys)
         while let fileURL = enumerator?.nextObject() as? URL {
             if fileURL.lastPathComponent == ".DS_Store" {
                 try? fileManager.removeItem(at: fileURL)
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# DS-Store-Remover
+<h1 align="center">
+  <img src="DS-Store-Remover/Assets.xcassets/AppIcon.appiconset/Untitled-macOS-Default-512x512@2x.png" alt="DS-Store-Remover" width="128" />
+</h1>
+
+<h3 align="center">
+DS-Store-Remover for macOS
+</h3>
+
+## Preview
+![](Image1.png)
+![](Image2.png)
+
+## Other Methods to Remove .DS_Store Files
+```bash
+cd documents
+find . -name .DS_Store -type f -delete
+```
